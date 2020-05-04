e7f922d0aeee29f9cbfa5cbe9f1462b0dc61773e
edarbieto <edarbieto@gmail.com>
2020-05-04 13:07:11 -0500

    Obtencion de IP corregida

--- web_server/app.py
+++ web_server/app.py
@@ -55,7 +55,7 @@ def notas():
         data_consulta['CARRERA'] = None
         data_consulta['OBS'] = e
     data_consulta['FECHA_HORA_FIN'] = datetime.datetime.now()
-    data_consulta['IP'] = request.environ['HTTP_X_REAL_IP']
+    data_consulta['IP'] = request.environ['HTTP_X_REAL_IP'].split(',')[0]
     add_consulta = ("""
     INSERT INTO CONSULTA
     (CODIGO_UPC, FECHA_HORA_INICIO, FECHA_HORA_FIN, IP, RESULTADO, NOMBRE, CARRERA, OBS)
